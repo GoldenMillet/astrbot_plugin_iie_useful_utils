c31d276dceb5c2b4e624a33f146a5ff2598ec655
下一站麦庄 <61528258+GoldenMillet@users.noreply.github.com>
2025-07-28 20:49:03 +0800

    Add files via upload

---
+++ constants/C01_help_msgs.py
@@ -0,0 +1,31 @@
+# 基本信息
+C01_QQ_ID = 1978522237
+C01_QQ_ID_STR = f"{C01_QQ_ID}"
+C01_QQ_NICKNAME = "群机器人 - 墨提斯小姐"
+
+# /help帮助弹出的消息
+C01_HELP_MSG_F = f"""iie抄底群机器人微调工具箱:
+
+[常规]
+查看所有人的当前发言次数: /uuls
+清空某个人的数据: /uuclr <目标QQ号>
+清空所有人的数据: /uuclrall
+修改上下限: /uuset <新下限> <新上限>
+
+[资料]
+查看信工所导师信息(更新至2024年): /uuproflist
+查看信工所往年考情分析(更新至2024年): /uudetails
+"""
+
+# /uuclrall弹出消息
+C01_CLEAR_ALL_MSG = "已经重置本群内所有人的发言记数，现在大家又可以畅所欲言了！"
+
+# /ping弹出消息
+C01_PING_MSG = "嗨呀，这里是信工所学妹啦~不是杭高院的智能菇学姐啦~我可不会回答你pong哟~"
+
+# 输入了非法数据
+C01_INVALID_DATA_MSG = "输入的数据非法！"
+
+# 信工所导师名单与考情分析URL
+C01_PROFFESOR_LIST_URL = "http://ys-e.ysepan.com/wap/nextstationmnp/T9N3Lfb1RMAKhcFRcM/N8IBHd935EhRkHtNOal0PT95Bgp6PO/2024_prof_list.pdf"
+C01_EXAM_DETAILS_URL = "http://ys-e.ysepan.com/wap/nextstationmnp/T9N3Lfb1RMAKhcFRcM/N8IBHd935EhRHgtNPal0PT95Bhp6PO/2025_exam_report.pdf"
